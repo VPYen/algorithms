8ccdb659965634e7749db980bf1471e7236cce86
Vince <32404291+VPYen@users.noreply.github.com>
2018-12-19 13:24:40 -0800

    initial commit

--- twoSums.java
+++ twoSums.java
@@ -1,6 +1,6 @@
 /*
 Several people are standing in a row and need to be divided into two teams.
-The first person goes into team 1, the second goes into team 2,  
+The first person goes into team 1, the second goes into team 2,
 the third goes into team 1 again, the fourth into team 2, and so on. 
 
 You are given an array of positive integers - the weights of the people. Return an array of two integers,
